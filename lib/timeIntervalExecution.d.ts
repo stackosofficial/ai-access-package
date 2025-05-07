6cf9e82804c9da64144ba2041b287e16a2a1e033
AnkithVijay <vijay2ankith@gmail.com>
2025-05-07 14:50:33 +0530

    updated gitignore

--- lib/timeIntervalExecution.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TimeIntervalExecution {
-    lastInvokeTime: Date;
-    duration: number;
-    funcToRun: () => Promise<any>;
-    constructor(duration: number, funcToRun: () => Promise<any>);
-    execute: () => Promise<void>;
-}
